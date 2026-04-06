aabe090baa4e4be2138c2fa5e482aa9531587299
Kanikasubramaniam <jnpsubramaniam1974@gmail.com>
2026-04-06 16:02:48 +0530

    first commit

--- OneDrive/Documents/hcl/discount.java
+++ OneDrive/Documents/hcl/discount.java
@@ -1,7 +1,8 @@
+ import java.util.Scanner;
 public class discount  {
     public static void main(String[] args) {
 
-        double cartValue = 3000; // change value here
+        /*double cartValue = 3000; // change value here
 
         double discount = 0;
 
@@ -17,6 +18,27 @@ public class discount  {
 
         System.out.println("Cart Value: " + cartValue);
         System.out.println("Discount: " + discount);
-        System.out.println("Final Payable Amount: " + finalAmount);
+        System.out.println("Final Payable Amount: " + finalAmount);*/
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter distance (in km): ");
+        double distance = sc.nextDouble();
+
+        System.out.print("Enter mileage (km per liter): ");
+        double mileage = sc.nextDouble();
+
+        System.out.print("Enter fuel price (per liter): ");
+        double fuelPrice = sc.nextDouble();
+
+        // Calculation
+        double fuelNeeded = distance / mileage;
+        double totalCost = fuelNeeded * fuelPrice;
+
+        // Output
+        System.out.println("Fuel needed: " + fuelNeeded + " liters");
+        System.out.println("Total fuel cost: ₹" + totalCost);
+
+        sc.close();
     }
 }
